23ee1245f34b0c1293062a6342608c175fe11bb7
swapna <swapnakpearl0@gmail.com>
2018-10-08 17:02:12 +0530

    assertion demo

---
+++ AssertDemoTest.java
@@ -0,0 +1,26 @@
+package testing.org.LeelaMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AssertDemoTest {
+	
+	int i=11;	
+	@Test
+	public void demo()
+	{
+		Assert.assertEquals(true, i%2==0);
+		System.out.println("after assertion");
+	}
+	
+	@Test
+	public void demo1()
+	{
+		SoftAssert soft=new SoftAssert();
+		soft.assertEquals(true, i%2==0);
+		System.out.println("after assertion");
+	}
+
+
+}
